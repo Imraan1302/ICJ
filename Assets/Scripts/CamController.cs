026f155f8cf94671a726bd8363492cd696691f91
ImraanArenz <85824978+Imraan1302@users.noreply.github.com>
2021-07-29 21:22:57 +0200

    Basic character stuff

---
+++ Assets/Scripts/CamController.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour
+{
+    public static CamController camcont;
+    public float LookSensitivity = 100f;
+    public Transform PlyHitbox;
+    public RaycastHit LookHit;
+    public Vector3 test, sPoint, CamPosition;
+    public float xRotation = 0f;
+    public Camera main;
+    public float camZtilt, camtiltstep, camYtilt;
+
+
+
+    string FormatHours, FormatMin;
+    public float mouseX, mouseY, GrabRange, trueGrab, PrecisionRange;
+
+
+    void Awake()
+    {
+        camcont = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetMousePos();
+        RotateView();
+    }
+
+    void GetMousePos()
+    {
+        mouseX = Input.GetAxis("Mouse X") * LookSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * LookSensitivity * Time.deltaTime;
+    }
+
+
+
+    void RotateView()
+    {
+
+
+        xRotation -= mouseY;
+
+
+
+
+
+
+
+
+
+
+
+
+        PlyHitbox.Rotate(Vector3.up * mouseX);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+
+
+        
+
+        Physics.Raycast(CamPosition, transform.forward * 100, out LookHit, GrabRange);
+        // Debug.DrawRay(CamPosition, transform.forward * 100, Color.yellow);
+
+    }
+
+}
+
---
+++ Assets/Scripts/MoveCont.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCont : MonoBehaviour
+{
+    public GameObject groundcheck;
+    public bool ShiftDown, isGrounded, isCrouch , isMove;
+    public float groundDist, gConst, sprintMod, xVal, yVal, moveSpeed;
+    public LayerMask groundMask;
+    public Vector3 downVelocity, move;
+    public Rigidbody rig;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+
+        rig = this.GetComponent<Rigidbody>();
+
+        ShiftDown = false;
+        groundDist = 0.2f;
+        gConst = -9.8f;
+        sprintMod = 45f;
+
+
+    }
+
+    void FixedUpdate()
+    {
+        HandleMovePly();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMoveInputs();
+        Sprint();
+        Crouch();
+    }
+
+    void HandleMoveInputs()
+    {
+
+
+        isGrounded = Physics.CheckSphere(groundcheck.transform.position, groundDist, groundMask);
+
+
+
+
+
+        xVal = Input.GetAxis("Horizontal");
+        yVal = Input.GetAxis("Vertical");
+
+
+
+
+        if ((yVal != 0))
+        {
+            isMove = true;
+        }
+        else
+        {
+            isMove = false;
+        }
+
+
+
+        if (isGrounded && downVelocity.y < 0)
+        {
+            downVelocity.y = -200f;
+        }
+
+        //end of gravity
+
+        move = transform.right * xVal + transform.forward * yVal;
+
+
+    }
+
+    void HandleMovePly()
+    {
+
+        if (!isCrouch)
+        {
+            if (ShiftDown)
+            {
+
+                rig.velocity = (move * moveSpeed * (1 + (sprintMod / 100)) * Time.deltaTime) + (downVelocity * Time.deltaTime);
+            }
+            else
+            {
+                rig.velocity = (move * moveSpeed * Time.deltaTime) + (downVelocity * Time.deltaTime);
+            }
+        }
+        else
+        {
+            rig.velocity = (move * moveSpeed * Time.deltaTime * .6f) + (downVelocity * Time.deltaTime);
+        }
+    }
+
+    void Sprint()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            ShiftDown = !ShiftDown;
+        }
+
+        if (!isMove)
+        {
+            ShiftDown = false;
+        }
+    }
+
+    void Crouch()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        { 
+            isCrouch = true;
+        }
+
+        if(Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            isCrouch = false;
+        }
+    }
+}

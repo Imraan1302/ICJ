525d5e798f643fa4b2c8ef1aa9eb4e6da95a122b
Cheyenne-Peter <2180052@students.wits.ac.za>
2021-07-30 13:03:18 +0200

    CheyenneCommit

--- Assets/Scripts/CamController.cs
+++ Assets/Scripts/CamController.cs
@@ -26,7 +26,7 @@ public class CamController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.lockState = CursorLockMode.Locked;
 
     }
 
